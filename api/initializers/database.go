c76445fef712e129c1f6533a939c7251d3b5cb4b
maximmikhailov1 <max.mikhailov2003@yandex.ru>
2024-10-23 03:50:27 +0300

    added new option on gorm.Config

--- api/initializers/database.go
+++ api/initializers/database.go
@@ -13,7 +13,10 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 	dsn := os.Getenv("DB_URL")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{TranslateError: true})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		TranslateError:         true,
+		SkipDefaultTransaction: false, //Speeds up operations if set to true, but doesnt write an operation in log
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to database")
 	}
