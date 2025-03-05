d89794c006d33f1f05f6adf1935e738bad320e63
Maxim Mikhailov <max.mikhailov2003@yandex.ru>
2025-03-05 03:01:08 +0300

    enabled working on local and fixed security and nav

--- frontend/labs-front/components/Layout.tsx
+++ frontend/labs-front/components/Layout.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react"
 import { useRouter, usePathname} from "next/navigation"
 import Navigation from "./Navigation"
 import Home from "./Home"
-import {checkAuth, getUser} from "@/app/actions/auth"
+import {checkAuthAndRole, getUser} from "@/app/actions/auth"
 import { ProfilePage } from "./profile/profile-page"
 import AuthPage from "./AuthPage"
 import SubjectManagement from "./teacher/SubjectManagement"
@@ -22,7 +22,7 @@ interface LayoutProps {
 
 
 const Layout: React.FC<LayoutProps> = ({ searchParams }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [currentPage, setCurrentPage] = useState("home")
   const [isLoading, setIsLoading] = useState(true)
   const [userRole, setUserRole] = useState<"student" | "tutor" | null>(null)
@@ -32,16 +32,14 @@ const Layout: React.FC<LayoutProps> = ({ searchParams }) => {
 
   useEffect(() => {
     const initialize = async () => {
-      const loggedInStatus = localStorage.getItem("isLoggedIn")
-      const storedUserRole = JSON.stringify(getAndSetUserRole()) as "student" | "tutor" | null | unknown
+      const { isAuthenticated, storedRole } = await checkAuthAndRole()
       const lastPage = localStorage.getItem("lastPage")
   
-      if (loggedInStatus === "true" && storedUserRole) {
-        setIsLoggedIn(true)
-        setUserRole(userRole)
-
+      if (isAuthenticated && storedRole) {
+        setIsAuthenticated(true)
+        setUserRole(storedRole)
         // Перенаправление преподавателей на страницу расписания
-        if (storedUserRole === "tutor" && (pathname === "/" || pathname === "/home")) {
+        if (storedRole === "tutor" && (pathname === "/" || pathname === "/home")) {
           router.replace("/all-teachers-schedule")
           setCurrentPage("all-teachers-schedule")
           return
@@ -50,14 +48,14 @@ const Layout: React.FC<LayoutProps> = ({ searchParams }) => {
         if (pathname === "/auth") {
           const params = new URLSearchParams(searchParams)
           const callbackUrl = params.get("callbackUrl")
-          router.replace(callbackUrl || lastPage || (storedUserRole === "tutor" ? "/all-teachers-schedule" : "/"))
+          router.replace(callbackUrl || lastPage || (storedRole === "tutor" ? "/all-teachers-schedule" : "/"))
         }
       } else {
         if (pathname !== "/auth") {
           localStorage.setItem("lastPage", pathname)
           router.replace("/auth")
         }
-        setIsLoggedIn(false)
+        setIsAuthenticated(false)
         setUserRole(null)
       }
   
@@ -69,23 +67,19 @@ const Layout: React.FC<LayoutProps> = ({ searchParams }) => {
     initialize()
   }, [pathname, router, searchParams])
 
-  const getAndSetUserRole = async () => {
-    try {
-      const result = await checkAuth()
-      if (result.success && result.userRole){
-        return result.userRole
-      }
-    } catch (error) {
-      console.error("Ошибка загрузки данных пользователя:", error)
-      toast.error("Попытка перейти на страницу, которая не предназначается пользователю")
-    }
-  }
 
   const handleLogin = async (role: "student" | "tutor") => {
-    setIsLoggedIn(true)
-    setUserRole(role)
-    localStorage.setItem("isLoggedIn", "true")
-    
+    const { isAuthenticated, storedRole } = await checkAuthAndRole()
+
+    console.log(isAuthenticated, storedRole)
+    if (!isAuthenticated || storedRole !== role) {
+      toast.error("Ошибка авторизации")
+      return
+    }
+
+    setIsAuthenticated(true)
+    setUserRole(storedRole)
+
     try {
       const result = await getUser()
       if (result.success && result.user) {
@@ -108,17 +102,16 @@ const Layout: React.FC<LayoutProps> = ({ searchParams }) => {
   }
 
   const handleLogout = () => {
-    setIsLoggedIn(false)
+    setIsAuthenticated(false)
     setUserRole(null)
     document.cookie = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    localStorage.removeItem("isLoggedIn")
     localStorage.removeItem("lastPage")
     localStorage.removeItem("userProfile")
     router.replace("/auth",{scroll:false})
   }
 
   const handlePageChange = (page: string) => {
-    if (!isLoggedIn && page !== "auth") {
+    if (!isAuthenticated && page !== "auth") {
       localStorage.setItem("lastPage", `/${page}`)
       router.replace("/auth", { scroll: false })
       return
@@ -126,15 +119,14 @@ const Layout: React.FC<LayoutProps> = ({ searchParams }) => {
   
     // Сначала обновляем состояние
     setCurrentPage(page)
-    //TODO: ГДЕ ТО ТУТ БАГУЛИНА
-    // Затем выполняем навигацию
+
     const path = page === "home" ? "/" : `/${page}`
     localStorage.setItem("lastPage", path)
     router.replace(path, { scroll: false })
   }
 
   const renderContent = () => {
-    if (!isLoggedIn || currentPage === "auth") {
+    if (!isAuthenticated || currentPage === "auth") {
       return <AuthPage onLogin={handleLogin} />
     }
 
@@ -189,7 +181,7 @@ const Layout: React.FC<LayoutProps> = ({ searchParams }) => {
         <div className="space-y-8 h-full mx-auto p-0">
             <div className=" bg-gray-50 min-h-full">
               <Navigation
-                isLoggedIn={isLoggedIn}
+                isLoggedIn={isAuthenticated}
                 onLogout={handleLogout}
                 setCurrentPage={handlePageChange}
                 userRole={userRole}
